cc1f1a30a32f01063af5e0b483c7ced5e113eb75
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-11-21 12:19:26 -0300

    Atualizando HTML

--- js/validation.js
+++ js/validation.js
@@ -1,92 +1,67 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const emailConfirmation = document.getElementById("email-confirmation");
-const password = document.getElementById("password");
-const passwordConfirmation = document.getElementById("password-confirmation");
+const form   = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans  = document.querySelectorAll('.span-required');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    checkInputs();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    mainPasswordValidate();
+    comparePassword();
 });
 
-function checkInputs(){
-    const usernameValue = username.value;
-    const emailValue = email.value;
-    const emailConfirmationValue = emailConfirmation.value;
-    const passwordValue = password.value;
-    const passwordConfirmationValue = passwordConfirmation.value;
-
-    if(usernameValue === ""){
-        setErrorFor(username, "O nome de usuário é obrigatório!");
-    }else if(usernameValue.length < 3){      
-        setErrorFor(username, "O nome precisa ter no mínimo 3 caracteres! ")
-    }else{
-        setSuccessFor(username);
-    }
-
-    if(emailValue === ""){
-        setErrorFor(email,"O email é obrigatório");
-    }else if(!checkEmail(emailValue)){
-        setErrorFor(email,"Por favor, digite um email válido!");
-    }else{
-        setSuccessFor(email)
-    }
-
-    if(emailConfirmationValue === ""){
-        setErrorFor(emailConfirmation,"A confirmação de e-mail é obrigatória!");
-    }else if(emailValue !== emailConfirmationValue){
-        setErrorFor(emailConfirmation, "Email não confere")
-    }else{
-        setSuccessFor(emailConfirmation);
-    }
-
-    if(passwordValue === ""){
-        setErrorFor(password,"A senha é obrigatória!");
-    }else if(passwordValue.length < 8){
-        setErrorFor(password,"A senha tem que ter no mínimo 8 caracteres!")
-    }else{
-        setSuccessFor(password);
-    }
-
-    if(passwordConfirmationValue === ""){
-        setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória!");
-    }else if(passwordValue !== passwordConfirmationValue){
-        setErrorFor(passwordConfirmation,"As senhas digitadas não conferem!");
-    }else{
-        setSuccessFor(passwordConfirmation);
-    }
-
-    const formControls = form.querySelectorAll(".form-control");
-
-    const formIsValid = [...formControls].every((formControl) => {
-        return formControl.className === "form-control success";
-    });
-
-    if (formIsValid) {
-    console.log("O formulário está 100% válido!");
-    }
-
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
 }
 
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-
-    small.innerText = message;//mensagem de erro
-    formControl.className = "form-control error";
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
 }
 
-function setSuccessFor(input){
-    const formControl = input.parentElement;
-
-    formControl.className = "form-control success";//mensagem de sucesso
+function nameValidate(){
+    if(campos[0].value.length < 3)
+    {
+        setError(0);
+    }
+    else
+    {
+        removeError(0);
+    }
 }
 
-function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    );
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else
+    {
+        removeError(1);
+    }
 }
-  
+
+function mainPasswordValidate(){
+    if(campos[2].value.length < 8)
+    {
+        setError(2);
+    }
+    else
+    {
+        removeError(2);
+        comparePassword();
+    }
+}
+
+function comparePassword(){
+    if(campos[2].value == campos[3].value && campos[3].value.length >= 8)
+    {
+        removeError(3);
+    }
+    else
+    {
+        setError(3);
+    }
+}
