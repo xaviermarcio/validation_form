aeef5e8273ef6db8c272fada74d6f5188c5a73da
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-11-21 13:18:00 -0300

    Finalizando atualizações

--- js/validation.js
+++ js/validation.js
@@ -1,67 +1,40 @@
-const form   = document.getElementById('form');
-const campos = document.querySelectorAll('.required');
-const spans  = document.querySelectorAll('.span-required');
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
 const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    nameValidate();
-    emailValidate();
-    mainPasswordValidate();
-    comparePassword();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  nameValidate();
+  emailValidate();
+  mainPasswordValidate();
+  comparePassword();
 });
 
-function setError(index){
-    campos[index].style.border = '2px solid #e63636';
-    spans[index].style.display = 'block';
+function setError(index) {
+  campos[index].style.border = "3px solid #e63636";
+  spans[index].style.display = "block";
 }
 
-function removeError(index){
-    campos[index].style.border = '';
-    spans[index].style.display = 'none';
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
 }
 
-function nameValidate(){
-    if(campos[0].value.length < 3)
-    {
-        setError(0);
-    }
-    else
-    {
-        removeError(0);
-    }
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
 }
 
-function emailValidate(){
-    if(!emailRegex.test(campos[1].value))
-    {
-        setError(1);
-    }
-    else
-    {
-        removeError(1);
-    }
+function emailValidate() {
+  if (!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
 }
 
-function mainPasswordValidate(){
-    if(campos[2].value.length < 8)
-    {
-        setError(2);
-    }
-    else
-    {
-        removeError(2);
-        comparePassword();
-    }
-}
 
-function comparePassword(){
-    if(campos[2].value == campos[3].value && campos[3].value.length >= 8)
-    {
-        removeError(3);
-    }
-    else
-    {
-        setError(3);
-    }
-}
